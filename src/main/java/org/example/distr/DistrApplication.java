14aa48e4edd484ae395b9d7b85f28d7330106afd
romboooo <412902@niuitmo.ru>
2025-12-24 14:17:00 +0300

    first commit

---
+++ src/main/java/org/example/distr/DistrApplication.java
@@ -0,0 +1,13 @@
+package org.example.distr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/distr/DistrApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.distr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
